6c8a6fdbe1a00ef8b2e2b739b48c8464a9bd61fe
Andrew Wilkinson <andrew.wilkinson@usu.edu>
2024-04-25 14:29:57 -0600

    view recipes

--- client/src/ViewRecipes.jsx
+++ client/src/ViewRecipes.jsx
@@ -1,19 +1,15 @@
 import React, { useState, useEffect } from "react";
 import { useApi } from "./utils/use_api";
-import { useDispatch } from "react-redux";
-import { setAuthToken } from "./store/application_slice";
 import { useNavigate, Link } from "react-router-dom";
 
 export const ViewRecipes = () => {
     const [user, setUser] = useState(null);
     const [recipes, setRecipes] = useState([]);
     const api = useApi();
-    const navigate = useNavigate();
-    const dispatch = useDispatch();
 
     useEffect(() => {
         getUser();
-        // getRecipes();
+        getRecipes();
     }, []);
 
     async function getUser() {
@@ -24,11 +20,32 @@ export const ViewRecipes = () => {
             console.error("Error fetching user:", error);
         }
     };
-
+    async function getRecipes() {
+        try {
+          const { recipes } = await api.get("/recipes/me");
+          setRecipes(recipes);
+        } catch (error) {
+          console.error("Error fetching recipes:", error);
+        }
+      }
     return(
         <div className="container">
-            <div className="header">
-                <h1>I am on the View Recipes Page</h1>
+            <div className="recipe-list">
+                <h1>Your Recipes:</h1>
+                <ul>
+                    {recipes.map((recipe) => (
+                        <li key={recipe.id}>
+                            <div className="recipe-item">
+                                <h3 className="recipe-header-border">{recipe.name}</h3>
+                                <p className="recipe-border">Preperation Time: {recipe.prepTime} hours</p>
+                                <p className="recipe-border">Cooking Time: {recipe.cookingTime} hours</p>
+                                <p className="recipe-border">Serves: {recipe.servings} people</p>
+                                <p className="recipe-border">Ingredients: {recipe.ingredients}</p>
+                                <p className="recipe-border">Instructions: {recipe.instructions} </p>
+                            </div>
+                        </li>
+                    ))}
+                </ul>
             </div>
             <div>
                <Link className="action-button" to="../">Close Recipes</Link>
